31737610974a98c2a02bec3df85d187a387360ba
yusufyp <yusuf.yudhistirazhy@gmail.com>
2021-11-17 16:18:40 +0700

    type, alias and literal data type

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,7 +1,11 @@
-function combine(input1: number | string, input2: number | string){
+// make a new variabel to replace it with data type, good for union data type!
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor){
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-       result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+       result = +input1 + +input2;
     } else{
         result = input1.toString() + input2.toString();
     }
@@ -9,8 +13,13 @@ function combine(input1: number | string, input2: number | string){
 }
 
 // using number
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
+
+// string number
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
 // using string
-const combinedNames = combine('Yusuf', 'Yudhistira');
+const combinedNames = combine('Yusuf', 'Yudhistira', 'as-text');
 console.log(combinedNames);
